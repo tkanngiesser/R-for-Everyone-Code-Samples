2adfe8b1c0dbe4021a20b602142e25fe3d7cd89a
Tino Kanngießer <tinokanngiesser@Tinos-MBP.fritz.box>
2018-04-19 12:04:30 +0200

    chapter12 updated

--- chapter12.R
+++ chapter12.R
@@ -173,3 +173,29 @@ topN <- function(x, N=5)
 diamonds %>% 
   group_by(cut) %>% 
   do(topN(., N=3))
+
+diamonds %>% 
+  group_by(cut) %>% 
+  do(topN(., 3))
+
+#12.11 dplyr wirth Databases
+
+download.file('http://www.jaredlander.com/data/diamonds.db',
+              destfile = 'data/diamonds.db', mode = 'wb')
+
+diaDBSource <- src_sqlite('data/diamonds.db')
+diaDBSource
+
+diaDBSource2 <- DBI::dbConnect(RSQLite::SQLite(), 'data/diamonds.db')
+diaDBSource2
+
+diaTab <- tbl(diaDBSource, 'diamonds')
+diaTab
+
+diaTab %>% 
+  group_by(cut) %>% 
+  dplyr::summarize(Price=mean(price))
+
+diaTab %>% 
+  group_by(cut) %>% 
+  dplyr::summarize(Price=mean(price), Carat=mean(Carat))
